7ac4572298d2548e94f9329a1bb2f74d9baf7faa
liduqin <liduqin@qq.com>
2020-03-03 12:09:03 +0800

    # 提交考试结果 args[0]参数结果

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -8,7 +8,7 @@ public class AppRunner {
           2. 你写的程序将把testDataFile作为参数加载此文件并读取文件内的测试数据，并对每条测试数据计算结果。
           3. 将所有计费结果拼接并使用\n分割，然后保存到receipt变量中。
          */
-        String testDataFile = "src/main/resources/testData.txt";
+        String testDataFile = args[0];
         String receipt = CalPrint.getCalString(testDataFile);
         System.out.println(receipt);
     }
