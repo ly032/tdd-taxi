f83427ee852980f0f5db33d4224d56df6d8feade
李杜钦 <ly032@126.com>
2020-03-30 22:59:58 +0800

    1

--- src/test/java/com/jiker/keju/CalPrintTest.java
+++ src/test/java/com/jiker/keju/CalPrintTest.java
@@ -13,7 +13,7 @@ public class CalPrintTest {
         // 大于8公里,等待3分钟
         String testDataFile = "src/main/resources/testData.txt";
         String line = System.lineSeparator();
-        String testValue = String.format("收费6元" + line + "收费7元" + line + "收费13元" + line + "收费7元");
+        String testValue = String.format("收费7元" + line + "收费8元" + line + "收费21元");
         calTaxPricePrint(testDataFile, testValue);
     }
 
